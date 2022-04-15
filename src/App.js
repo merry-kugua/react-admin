3bd9f8b33fc0a8bcb7c615b9a2dff233b1c7cff6
merry-kugua <1819220834@qq.com>
2022-04-15 13:31:52 +0800

    react 配置styles文件提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.scss';
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import Home from './views/Home'
+import About from "./views/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+       <BrowserRouter>
+         <Switch>
+             <Route exact component={Home} path="/"></Route>
+             <Route component={About} path="/about"></Route>
+         </Switch>
+       </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import './Home.scss'
+class Home extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="texr">
+                <span className="aaa">111</span>
+            </div>
+        )
+    }
+}
+export default Home
